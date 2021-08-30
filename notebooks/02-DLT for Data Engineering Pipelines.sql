636ef240ea8d9ac0a526049b2b19f5a19fba8725
rlsalcido24 <rlsalcido24>
2021-08-30 23:28:27 +0000

    testtest

--- notebooks/02-DLT for Data Engineering Pipelines.sql
+++ notebooks/02-DLT for Data Engineering Pipelines.sql
@@ -3,6 +3,7 @@ CREATE INCREMENTAL LIVE TABLE customers
 COMMENT "The customers buying finished products, ingested from /databricks-datasets."
 TBLPROPERTIES ("quality" = "mapping")
 AS SELECT * FROM cloud_files("/databricks-datasets/retail-org/customers/", "csv")
+#comment here 
 
 -- COMMAND ----------
 
